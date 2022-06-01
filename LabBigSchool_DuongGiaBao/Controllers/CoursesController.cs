c60ee16a6fa097ec88cb9a0f9b9d118e0cd8b896
giabao2810 <giabao281001@gmail.com>
2022-06-01 11:08:24 +0700

    Add project files.

---
+++ LabBigSchool_DuongGiaBao/Controllers/CoursesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabBigSchool_DuongGiaBao.Controllers
+{
+    private readonly
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ LabBigSchool_DuongGiaBao/ViewModels/CourseViewModels.cs
@@ -0,0 +1,23 @@
+﻿using LabBigSchool_DuongGiaBao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LabBigSchool_DuongGiaBao.ViewModels
+{
+    public class CourseViewModels
+    {
+        public String Place { get; set; }
+        public String Date { get; set;}
+        public String Time { get; set; }
+
+        public byte Category { get; set; }
+        public IEnumerable <Category> categories { get; set; }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
